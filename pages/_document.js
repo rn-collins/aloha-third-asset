744f4882a7ae1e0bc3feb365ead69eb6ad2679b7
rn-collins <rayven.nikkita.collins@gmail.com>
2026-08-18 09:25:54 -1000

    Add branded favicon metadata

--- pages/_document.js
+++ pages/_document.js
@@ -1,2 +1,2 @@
 import {Html,Head,Main,NextScript} from 'next/document'
-export default function Document(){return <Html lang="en"><Head><meta name="theme-color" content="#102d28"/></Head><body><Main/><NextScript/></body></Html>}
+export default function Document(){return <Html lang="en"><Head><link rel="icon" type="image/svg+xml" href="/favicon.svg"/><meta name="theme-color" content="#102d28"/></Head><body><Main/><NextScript/></body></Html>}
